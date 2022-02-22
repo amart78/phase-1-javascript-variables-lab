08e469a275b0fe2179c3decd36ecf28fe5eb1fd1
Alexis <amart78@outlook.com>
2022-02-22 11:29:27 -0600

    commit tree 40e8b79881515aae8970615c364a09cb16213f40

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const companyName = 'Scuber'
+
+/*  mostProfitableNeighborhood
+1) is declared as equal to Chelsea
+2) is defined using let */
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+
+
+/*companyCeo
+3) is declared as equal to Susan Smith
+4) is defined using let */
+
+let companyCeo = 'Susan Smith'
